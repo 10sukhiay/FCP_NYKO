1641c7a618f8ee8ee7a3cd72878effa9154e5ccd
10sukhiay <73694548+10sukhiay@users.noreply.github.com>
2021-04-11 11:45:25 +0100

    syntax fix

--- Initial.py
+++ Initial.py
@@ -73,15 +73,15 @@ class COVID_MAP:
         axes[1].set_ylim(0, self.ysize)
 
 #POSITION AND MOVEMENT
-    def Move(position_state):
-        movement = np.random.randint(-1, 2, size=(
-        pop_size, 2))  # creates a random array of -1 to 1 to add to the current positions
-        coords = position_state[:, 0:2] + movement
-        coords[coords < 1] = 1  # prevent movement below x and y axis
-        coords[coords[:, 0] > (xsize - 2)] = (xsize - 2)  # prevent movement beyond x axis
-        coords[coords[:, 1] > (ysize - 2)] = (ysize - 2)  # prevent movement beyond y axis
-        position_state[:, 0:2] = coords
-        return position_state
+def Move(position_state):
+    movement = np.random.randint(-1, 2, size=(
+    pop_size, 2))  # creates a random array of -1 to 1 to add to the current positions
+    coords = position_state[:, 0:2] + movement
+    coords[coords < 1] = 1  # prevent movement below x and y axis
+    coords[coords[:, 0] > (xsize - 2)] = (xsize - 2)  # prevent movement beyond x axis
+    coords[coords[:, 1] > (ysize - 2)] = (ysize - 2)  # prevent movement beyond y axis
+    position_state[:, 0:2] = coords
+    return position_state
 
 #TRANSMITION FUNCTION
 def Transmission(position_state, heat):
