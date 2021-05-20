8e7627e1e186dc77d32eb1528ac7be4e9c94c0c7
KMelville777 <73698484+KMelville777@users.noreply.github.com>
2021-05-20 17:52:32 +0100

    Clean code

--- Simulation Environment/V10.1 Separate Files/animate.py
+++ Simulation Environment/V10.1 Separate Files/animate.py
@@ -8,17 +8,18 @@ May 2021
 
 """
 
-#standard imports
+# standard imports
 import numpy as np
 import pandas as pd
 from matplotlib import animation
 from matplotlib.animation import FuncAnimation
 from matplotlib import pyplot as plt
 
-#custom imports
+# custom imports
 from update import update
 
-def animate(people, heat_maps, position_state, rooms, days, day_length, G, number_nodes, limit, death_rate):
+
+def animate(people, heat_maps, position_state, rooms, days, day_length, g, number_nodes, limit, death_rate):
 
     # colours for statuses in scatter plot
     colour_dict = dict({1: 'green',
@@ -33,24 +34,26 @@ def animate(people, heat_maps, position_state, rooms, days, day_length, G, numbe
 
     plt.rc('font', **font)
 
-    fig, axes = plt.subplots(2, rooms+1, figsize=(3.5*(rooms+1), 7), sharey=False, sharex=False)
+    fig, axes = plt.subplots(2, rooms+1, figsize=(3.5*(rooms+1), 7))
 
-    Susceptible = []
-    Infected = []
-    Infectious = []
-    Recovered = []
-    Deceased = []
+    susceptible = []
+    infected = []
+    infectious = []
+    recovered = []
+    deceased = []
 
-    Room1 = []
-    Room2 = []
-    Room3 = []
+    room1 = []
+    room2 = []
+    room3 = []
 
     anim = FuncAnimation(fig=fig, func=update,
                          frames=day_length*days,
-                         fargs=(people, heat_maps, position_state, axes, colour_dict, day_length, G, number_nodes, limit, death_rate, Susceptible, Infected, Infectious, Recovered, Deceased, Room1, Room2, Room3),
+                         fargs=(people, heat_maps, position_state, axes, colour_dict, day_length, g, number_nodes,
+                                limit, death_rate,
+                                susceptible, infected, infectious, recovered, deceased,
+                                room1, room2, room3),
                          interval=50,
                          blit=False,
                          repeat=False)
-
 
     plt.show()
