bd5b405d7a7546fdb68223101dca16accdd21b52
10sukhiay <73694548+10sukhiay@users.noreply.github.com>
2021-05-20 10:22:41 +0100

    animation of line graph

--- Simulation Environment/V10 Separate Files/animate.py
+++ Simulation Environment/V10 Separate Files/animate.py
@@ -26,10 +26,17 @@ def animate(people, heat_maps, position_state, rooms, days, day_length, G, numbe
                        4: 'blue',
                        5: 'black'})
     grid_kws = {'width_ratios': (0.9, 0.05), 'wspace': 0.2}
-    fig, axes = plt.subplots(2, rooms, figsize=(5*rooms, 10), sharey=True, sharex=True)
+    fig, axes = plt.subplots(2, rooms+1, figsize=(5*(rooms+1), 10), sharey=False, sharex=False)
+
+    Susceptible = []
+    Infected = []
+    Infectious = []
+    Recovered = []
+    Deceased = []
+
     anim = FuncAnimation(fig=fig, func=update,
                          frames=day_length*days,
-                         fargs=(people, heat_maps, position_state, axes, colour_dict, day_length, G, number_nodes, limit, death_rate),
+                         fargs=(people, heat_maps, position_state, axes, colour_dict, day_length, G, number_nodes, limit, death_rate, Susceptible, Infected, Infectious, Recovered, Deceased),
                          interval=50,
                          blit=False,
                          repeat=False)
